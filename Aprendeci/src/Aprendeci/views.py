43b51ec6618eccb58db0353cc04bda8d25145484
Daniel Jiménez <danijimenez010@gmail.com>
2014-04-23 22:53:44 -0500

    [Tutorial] Templates and App Creation

--- Aprendeci/src/Aprendeci/views.py
+++ Aprendeci/src/Aprendeci/views.py
@@ -1,13 +1,19 @@
-from django.http import HttpResponse
+#from django.template.loader import get_template
+#from django.template import Context
+#from django.http import HttpResponse
 import datetime
+from django.shortcuts import render_to_response
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    current_date = datetime.datetime.now()
+    #t = get_template('current_datetime.html')
+    #html = t.render(Context({'current_date': now}))
+    #return HttpResponse(html)
+    return render_to_response('current_datetime.html', locals())
 
 def hours_ahead(request, offset):
-    offset = int(offset)
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    hour_offset = int(offset)
+    next_time = datetime.datetime.now() + datetime.timedelta(hours=hour_offset)
+    #html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    #return HttpResponse(html)
+    return render_to_response('hours_ahead.html', locals())
